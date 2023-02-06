480ff73b6e2b4ae3ee0217f2081701160a048d58
aaron GitHub <aaronse@outlook.com>
2023-02-05 18:35:49 -0800

    Minor.  Missed // AZA comment for BAUDRATE_2 115200

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -125,7 +125,7 @@
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
 #define SERIAL_PORT_2 3 // AZA 3 = Wifi module (limited to 115200? 250000 didn't work)
-#define BAUDRATE_2 115200   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
+#define BAUDRATE_2 115200   // AZA :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 /**
  * Select a third serial port on the board to use for communication with the host.
